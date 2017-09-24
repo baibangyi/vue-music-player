3fefc414a900df93b91b73af80fe2339873f9bd9
baibangyi <15808022614@163.com>
2017-09-25 00:17:50 +0800

    recommend and singerdata

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+     <m-header></m-header>
+     <tab></tab>
+     <keep-alive>
+        <router-view></router-view>
+     </keep-alive>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import MHeader from 'components/mHeader/mHeader'
+import Tab from 'components/tab/tab'
+export default {
+  name: 'app',
+  components: {
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style scpoed lang="stylus" rel="stylesheet/stylus">
+ 
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import fastclick from 'fastclick'
+import router from './router'
+import VueLazyload from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
