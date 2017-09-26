93a7a3c4a0c9089fa4de9cbb9a7bae3c9f95ffe8
baibangyi <15808022614@163.com>
2017-09-26 22:22:38 +0800

    get singer detail data and start music-list component

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,6 +1,7 @@
 <template>
   <scroll class="listview" 
-  :data="data" ref="listview" 
+  :data="data" 
+  ref="listview" 
   :listen-scroll="listenScroll" 
   @scroll="scroll" 
   :probe-type="probeType">
@@ -10,7 +11,7 @@
       ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
     		<ul>
-  				<li v-for="item in group.items" class="list-group-item">
+  				<li @click="selectItem(item)" v-for="item in group.items" class="list-group-item">
   					<img class="avatar" v-lazy="item.avatar"/>
   					<span class="name">{{item.name}}</span>
   				</li>
@@ -82,6 +83,9 @@
       }
     },
     methods: {
+      selectItem(item) {
+        this.$emit('select', item)
+      },
       onShortcutTouchStart(e) {
         let anchorIndex = getData(e.target, 'index')
         let firstTouch = e.touches[0]
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
 import VueLazyload from 'vue-lazyload'
+import store from './store'
 
 import 'common/stylus/index.styl'
 
@@ -17,5 +18,6 @@ Vue.use(VueLazyload, {
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
