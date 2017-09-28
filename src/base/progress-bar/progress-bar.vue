9de207dbbf5617c4a900064d6ec6b1ad9841da6c
baibangyi <15808022614@163.com>
2017-09-28 22:41:33 +0800

    finish song world page

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="progress-bar" ref="progressBar" @click="progressClick">
-		<div class="bar-inner">
-			<div class="progress" ref="progress"></div>
-			<div class="progress-btn-wrapper" 
-			     ref="progressBtn" 
-			     @touchstart.prevent="progressYouchStart"
-			     @touchmove.prevent="progressTouchMove"
-			     @touchend="progressTouchEnd">
-				<div class="progress-btn"></div>
-			</div>
-		</div>
-	</div>
+  <div class="progress-bar" ref="progressBar" @click="progressClick">
+    <div class="bar-inner">
+      <div class="progress" ref="progress"></div>
+      <div class="progress-btn-wrapper" ref="progressBtn"
+           @touchstart.prevent="progressTouchStart"
+           @touchmove.prevent="progressTouchMove"
+           @touchend="progressTouchEnd"
+      >
+        <div class="progress-btn"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -30,7 +30,7 @@
       this.touch = {}
     },
     methods: {
-      progressYouchStart(e) {
+      progressTouchStart(e) {
         this.touch.initiated = true
         this.touch.startX = e.touches[0].pageX
         this.touch.left = this.$refs.progress.clientWidth
@@ -47,23 +47,27 @@
         this.touch.initiated = false
         this._triggerPercent()
       },
-      _offset(offsetWidth) {
-        this.$refs.progress.style.width = `${offsetWidth}px`
-        this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+      progressClick(e) {
+        const rect = this.$refs.progressBar.getBoundingClientRect()
+        const offsetWidth = e.pageX - rect.left
+        this._offset(offsetWidth)
+        // 这里当我们点击 progressBtn 的时候，e.offsetX 获取不对
+        // this._offset(e.offsetX)
+        this._triggerPercent()
       },
       _triggerPercent() {
         const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
         const percent = this.$refs.progress.clientWidth / barWidth
         this.$emit('percentChange', percent)
       },
-      progressClick(e) {
-        this._offset(e.offsetX)
-        this._triggerPercent()
+      _offset(offsetWidth) {
+        this.$refs.progress.style.width = `${offsetWidth}px`
+        this.$refs.progressBtn.style[transform] = `translate3d(${offsetWidth}px,0,0)`
       }
     },
     watch: {
       percent(newPercent) {
-        if (newPercent >= 0 && this.touch.initiated) {
+        if (newPercent >= 0 && !this.touch.initiated) {
           const barWidth = this.$refs.progressBar.clientWidth - progressBtnWidth
           const offsetWidth = newPercent * barWidth
           this._offset(offsetWidth)
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -17,16 +17,32 @@
 				<h1 class="title" v-html="currentSong.name"></h1>
 				<h2 class="subtitle" v-html="currentSong.singer"></h2>
 			</div>
-			<div class="middle">
-				<div class="middle-l">
+			<div class="middle"
+            @touchstart.prevent="middleTouchStart"
+            @touchmove.prevent="middleTouchMove"
+            @touchend="middleTouchEnd">
+				<div class="middle-l" ref="middleL">
 					<div class="cd-wrapper" ref="cdWrapper">
 						<div class="cd" :class="cdCls">
 							<img class="image" :src="currentSong.image"/>
 						</div>
 					</div>
 				</div>
+        <div class="middle-r" ref="lyricList">
+          <div class="lyric-wrapper">
+            <div v-if="currentLyric">
+              <p ref="lyricLine" class="text"
+              :class="{'current': currentLineNum ===index}"
+              v-for="(line,index) in currentLyric.lines">{{line.txt}}</p>
+            </div>
+          </div>
+        </div>
 			</div>
 			<div class="bottom">
+        <div class="dot-wrapper">
+          <span class="dot" :class="{'active': currentShow === 'cd'}"></span>
+          <span class="dot" :class="{'active': currentShow === 'lyric'}"></span>
+        </div>
         <div class="progress-wrapper">
           <span class="time time-l">{{format(currentTime)}}</span>
           <div class="progress-bar-wrapper">
@@ -92,15 +108,20 @@
   import ProgressCircle from 'base/progress-circle/progress-circle'
   import {playMode} from 'common/js/config'
   import {shuffle} from 'common/js/util'
+  import Lyric from 'lyric-parser'
 
   const transform = prefixStyle('transform')
+  const transitionDuration = prefixStyle('transitionDuration')
 
   export default {
     data() {
       return {
         songReady: false,
         currentTime: 0,
-        radius: 32
+        radius: 32,
+        currentLyric: null,
+        currentLineNum: 0,
+        currentShow: 'cd'
       }
     },
     computed: {
@@ -131,6 +152,9 @@
         'mode',
         'sequenceList'
       ])
+    },
+    created() {
+      this.touch = {}
     },
     methods: {
       next() {
@@ -244,6 +268,18 @@
         const second = this._pad(interval % 60)
         return `${minute}:${second}`
       },
+      getLyric() {
+        this.currentSong.getLyric().then((lyric) => {
+          this.currentLyric = new Lyric(lyric, this.handleLyric)
+          console.log(this.currentLyric)
+          if (this.playing) {
+            this.currentLyric.play()
+          }
+        })
+      },
+      handleLyric({lineNum, txt}) {
+        this.currentLineNum = lineNum
+      },
       _pad(num, n = 2) {
         let len = num.toString().length
         while (len < n) {
@@ -270,6 +306,59 @@
         })
         this.setCurrentIndex(index)
       },
+      middleTouchStart(e) {
+        this.touch.initiated = true
+        const touch = e.touches[0]
+        this.touch.startX = touch.pageX
+        this.touch.startY = touch.pageY
+      },
+      middleTouchMove(e) {
+        if (!this.touch.initiated) {
+          return
+        }
+        const touch = e.touches[0]
+        const deltaX = touch.pageX - this.touch.startX
+        const deltaY = touch.pageY - this.touch.startY
+        if (Math.abs(deltaY) > Math.abs(deltaX)) {
+          return
+        }
+        const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+        this.touch.percent = Math.abs(offsetWidth / window.innerWidth)
+        this.$refs.lyricList.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.style[transitionDuration] = 0
+        this.$refs.middleL.style.opacity = 1 - this.touch.percent
+        this.$refs.middleL.style[transitionDuration] = 0
+      },
+      middleTouchEnd() {
+        let offsetWidth
+        let opacity
+        if (this.currentShow === 'cd') {
+          if (this.touch.percent > 0.1) {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+            this.currentShow = 'lyric'
+          } else {
+            offsetWidth = 0
+            opacity = 1
+          }
+        } else {
+          if (this.touch.percent < 0.9) {
+            offsetWidth = 0
+            this.currentShow = 'cd'
+            opacity = 1
+          } else {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+          }
+        }
+
+        const time = 300
+        this.$refs.lyricList.style[transform] = `translate3d(${offsetWidth}px, 0, 0)`
+        this.$refs.lyricList.style[transitionDuration] = `${time}ms`
+        this.$refs.middleL.style.opacity = opacity
+        this.$refs.middleL.style[transitionDuration] = `${time}ms`
+      },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN',
         setPlayingState: 'SET_PLAYING_STATE',
@@ -285,6 +374,7 @@
         }
         this.$nextTick(() => {
           this.$refs.audio.play()
+          this.getLyric()
         })
       },
       playing(newPlaying) {
