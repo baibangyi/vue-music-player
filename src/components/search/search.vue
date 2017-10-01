cac6ba676573b0b5ae4e3ee17161d49dd4ba6b06
baibangyi <15808022614@163.com>
2017-10-01 18:47:25 +0800

    finish search require

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,17 +1,60 @@
 <template>
-	<div class="search">
-		<div class="search-box-wrapper">
-      <search-box></search-box>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box ref="searchBox" @query="onQueryChange"></search-box>
+  	</div>
+		<div class="shortcut-wrapper" v-show="!query">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+          	<li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
+          		<span>{{item.k}}</span>
+          	</li>
+          </ul>
+        </div>
+      </div>
 		</div>
+    <div class="search-result" v-show="query">
+      <suggest :query="query"></suggest>
+    </div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
   import SearchBox from 'base/search-box/search-box'
+  import {getHotKey} from 'api/search'
+  import {ERR_OK} from 'api/config'
+  import Suggest from 'components/suggest/suggest'
 
   export default {
+    data() {
+      return {
+        hotKey: [],
+        query: ''
+      }
+    },
+    created() {
+      this._getHotKey()
+    },
+    methods: {
+      _getHotKey() {
+        getHotKey().then((res) => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      },
+      addQuery(query) {
+        this.$refs.searchBox.setQuery(query)
+      },
+      onQueryChange(query) {
+        this.query = query
+      }
+    },
     components: {
-      SearchBox
+      SearchBox,
+      Suggest
     }
   }
 </script>
---
+++ src/components/suggest/suggest.vue
@@ -0,0 +1,157 @@
+<template>
+  <scroll class="suggest" 
+  :data="result" 
+  :pullup="pullup"
+  @scrollToEnd="searchMore"
+  ref="suggest">
+    <ul class="suggest-list">
+      <li class="suggest-item" v-for="item in result">
+        <div class="icon">
+          <i :class="getIconCls(item)"></i>
+        </div>
+        <div class="name">
+          <p class="text" v-html="getDisplayName(item)"></p>
+        </div>
+      </li>
+      <loading v-show="hasMore" title=""></loading>
+    </ul>
+  </scroll>
+</template>
+
+<script type="text/ecmascript-6">
+  import {search} from 'api/search'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+
+  const TYPE_SINGER = 'singer'
+  const perpage = 20
+
+  export default {
+    data() {
+      return {
+        page: 1,
+        result: [],
+        pullup: true,
+        hasMore: true
+      }
+    },
+    props: {
+      query: {
+        type: String,
+        default: ''
+      },
+      showSinger: {
+        type: Boolean,
+        default: true
+      }
+    },
+    methods: {
+      search() {
+        this.page = 1
+        this.hasMore = true
+        this.$refs.suggest.scrollTo(0, 0)
+        search(this.query, this.page, this.showSinger, perpage).then((res) => {
+          if (res.code === ERR_OK) {
+            this.result = this._genResult(res.data)
+            this._checkMore(res.data)
+          }
+        })
+      },
+      _genResult(data) {
+        let ret = []
+        if (data.zhida && data.zhida.singerid) {
+          ret.push({...data.zhida, ...{type: TYPE_SINGER}})
+        }
+        if (data.song) {
+          ret = ret.concat(this._normalizeSongs(data.song.list))
+        }
+        return ret
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          if (musicData.songid && musicData.albumid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      },
+      getIconCls(item) {
+        if (item.type === TYPE_SINGER) {
+          return 'icon-mine'
+        } else {
+          return 'icon-music'
+        }
+      },
+      getDisplayName(item) {
+        if (item.type === TYPE_SINGER) {
+          return item.singername
+        } else {
+          return `${item.name}-${item.singer}`
+        }
+      },
+      searchMore() {
+        if (!this.hasMore) {
+          return
+        }
+        this.page++
+        search(this.query, this.page, this.showSinger, perpage).then((res) => {
+          if (res.code === ERR_OK) {
+            this.result = this.result.concat(this._genResult(res.data))
+            this._checkMore(res.data)
+          }
+        })
+      },
+      _checkMore(data) {
+        const song = data.song
+        if (!song.list.length || (song.curnum + song.curpage * perpage) > song.totalnum) {
+          this.hasMore = false
+        }
+      }
+    },
+    watch: {
+      query() {
+        this.search()
+      }
+    },
+    components: {
+      Scroll,
+      Loading
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .suggest
+    height: 100%
+    overflow: hidden
+    .suggest-list
+      padding: 0 30px
+      .suggest-item
+        display: flex
+        align-items: center
+        padding-bottom: 20px
+      .icon
+        flex: 0 0 30px
+        width: 30px
+        [class^="icon-"]
+          font-size: 14px
+          color: $color-text-d
+      .name
+        flex: 1
+        font-size: $font-size-medium
+        color: $color-text-d
+        overflow: hidden
+        .text
+          no-wrap()
+    .no-result-wrapper
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
+</style>
