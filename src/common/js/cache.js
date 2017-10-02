bff2692633230103f9570cbb5cc8b2336d499263
baibangyi <15808022614@163.com>
2017-10-02 19:45:11 +0800

    solve bugs

--- src/common/js/cache.js
+++ src/common/js/cache.js
@@ -17,6 +17,13 @@ function insertArray(arr, val, compare, maxLen) {
   }
 }
 
+function deleteFromArray(arr, compare) {
+  const index = arr.findIndex(compare)
+  if (index > -1) {
+    arr.splice(index, 1)
+  }
+}
+
 export function saveSearch(query) {
   let searches = storage.get(SEARCH_KRY, [])
   insertArray(searches, query, (item) => {
@@ -24,4 +31,22 @@ export function saveSearch(query) {
   }, SEARCH_MAX_LENGTH)
   storage.set(SEARCH_KRY, searches)
   return searches
+}
+
+export function loadSearch() {
+  return storage.get(SEARCH_KRY, [])
+}
+
+export function deleteSearch(query) {
+  let searches = storage.get(SEARCH_KRY, [])
+  deleteFromArray(searches, (item) => {
+    return item === query
+  })
+  storage.set(SEARCH_KRY, searches)
+  return searches
+}
+
+export function clearSearch() {
+  storage.remove(SEARCH_KRY)
+  return []
 }
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -4,20 +4,36 @@
       <search-box ref="searchBox" @query="onQueryChange"></search-box>
   	</div>
 		<div class="shortcut-wrapper" v-show="!query">
-      <div class="shortcut">
-        <div class="hot-key">
-          <h1 class="title">热门搜索</h1>
-          <ul>
-          	<li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
-          		<span>{{item.k}}</span>
-          	</li>
-          </ul>
+      <scroll class="shortcut" :data="shortcut">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+            	<li @click="addQuery(item.k)" class="item" v-for="item in hotKey">
+            		<span>{{item.k}}</span>
+            	</li>
+            </ul>
+          </div>
+          <div class="search-history" v-show="searchHistory.length">
+            <h1 class="title">
+              <span class="text">搜索历史</span>
+              <span class="clear" @click="showConfirm">
+                <i class="icon-clear"></i>
+              </span>
+            </h1>
+            <search-list @select="addQuery" @delete="deleteSearchHistory" :searches="searchHistory"></search-list>
+          </div>
         </div>
-      </div>
+      </scroll>
 		</div>
     <div class="search-result" v-show="query">
       <suggest @select="saveSearch" @listScroll="blurInput" :query="query"></suggest>
     </div>
+    <confirm 
+    ref="confirm" 
+    text="是否清空所有历史"
+    confirmBtnText="清空"
+    @confirm="clearSearchHistory"></confirm>
     <router-view></router-view>
 	</div>
 </template>
@@ -27,8 +43,10 @@
   import {getHotKey} from 'api/search'
   import {ERR_OK} from 'api/config'
   import Suggest from 'components/suggest/suggest'
-  import {mapActions} from 'vuex'
+  import {mapActions, mapGetters} from 'vuex'
   import SearchList from 'base/search-list/search-list'
+  import Confirm from 'base/confirm/confirm'
+  import Scroll from 'base/scroll/scroll'
 
   export default {
     data() {
@@ -38,6 +56,9 @@
       }
     },
     computed: {
+      shortcut() {
+        return this.hotKey.concat(this.searchHistory)
+      },
       ...mapGetters([
         'searchHistory'
       ])
@@ -64,12 +85,22 @@
       },
       saveSearch() {
         this.saveSearchHistory(this.query)
-      }
+      },
+      showConfirm() {
+        this.$refs.confirm.show()
+      },
+      ...mapActions([
+        'saveSearchHistory',
+        'deleteSearchHistory',
+        'clearSearchHistory'
+      ])
     },
     components: {
       SearchBox,
       Suggest,
-      SearchList
+      SearchList,
+      Confirm,
+      Scroll
     }
   }
 </script>
